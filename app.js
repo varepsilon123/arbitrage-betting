dfb6b4efd1749a36e1b82a8acc894d9fc9b76ea1
varepsilon123 <jadetszting@gmail.com>
2023-05-19 13:25:10 -0700

    feat: display result in html

--- app.js
+++ app.js
@@ -3,11 +3,28 @@ const app = express();
 
 const { findHighestOdd } = require('./src/comparison/compare.js');
 
-app.get('/', async (req, res) => {
+app.get('/api', async (req, res) => {
     let a = await findHighestOdd();
-    res.json(a);
+
+    let HTMLcode = arr2html(a);
+    res.set('Content-Type', 'text/html');
+    res.send(HTMLcode);
 });
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
+
+function arr2html (data) {
+    let htmlCode = '<html><body>';
+
+    for (let i = 0; i < data.length; i++) {
+        Object.entries(data[i]).forEach(([key, value]) => {
+            htmlCode += `${key}: ${value}, `;
+          });
+          htmlCode += '<br/>';
+    }
+    htmlCode += '</body></html>';
+
+    return htmlCode;
+}
